663265e5a81029c731a05a47da6a9e285932ea1b
Meshackoo <116368127+Meshackoo@users.noreply.github.com>
2023-09-29 07:59:49 +0300

    Add files via upload

---
+++ SimpleCalculator/Meshack python.py
@@ -0,0 +1,7 @@
+#meshack otieno ouma sct211-0765/2022
+username=input("Enter your name:")
+print("Hello "+username)
+M1=input("Enter the first number:")
+M2=input("Enter the second number:")
+M3=float(M1)+float(M2)
+print("The sum of"+M1+" and "+M2+" is "+str(M3))
